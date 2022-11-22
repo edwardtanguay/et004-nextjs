599a047d6edfef23fe79ed6fe8a13c622b15d763
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-22 11:08:02 +0100

    header styled

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 function Header() {
 	return (
 		<header className="p-5 bg-blue-500">
-			<Link href="/">Home</Link>
-			<Link href="/nouns">Nouns</Link>
+			<Link className="mr-2 px-2 py-1 bg-white text-blue-500 rounded-lg" href="/">Home</Link>
+			<Link className="mr-2 px-2 py-1 bg-white text-blue-500 rounded-lg" href="/nouns">Nouns</Link>
 		</header>
 	);
 }
