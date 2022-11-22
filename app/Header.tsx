45b847af4d74a60bf2ca1989a7de7196e924340a
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-22 11:12:06 +0100

    colored buttons

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 function Header() {
 	return (
 		<header className="p-5 bg-blue-500">
-			<Link className="mr-2 px-2 py-1 bg-white text-blue-500 rounded-lg" href="/">Home</Link>
-			<Link className="mr-2 px-2 py-1 bg-white text-blue-500 rounded-lg" href="/nouns">Nouns</Link>
+			<Link className="mr-2 px-2 py-1 bg-orange-500 text-blue-500 rounded-lg" href="/">Home</Link>
+			<Link className="mr-2 px-2 py-1 bg-green-500 text-blue-500 rounded-lg" href="/nouns">Nouns</Link>
 		</header>
 	);
 }
