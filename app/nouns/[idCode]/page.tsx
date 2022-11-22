552e45f95c27ec71e757c9bc2acd7baa3724b494
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-22 11:54:44 +0100

    clicks work

--- app/nouns/[idCode]/page.tsx
+++ app/nouns/[idCode]/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import { Noun } from '../../../typings';
 
-function Noun({ params: { idCode } } : any) {
-  return (
-	  <div>Noun: {idCode}</div>
-  )
+const url = 'https://edwardtanguay.vercel.app/share/germanNouns.json';
+
+type Params = {
+	params: {
+		idCode: string;
+	};
+};
+
+async function Noun({ params: { idCode } }: Params) {
+	const nouns: Noun[] = (await axios.get(url)).data;
+	const rawNoun: Noun | undefined = nouns.find((m) => m.singular === idCode);
+	let noun: Noun =
+		rawNoun === undefined
+			? {
+					article: '',
+					singular: '',
+					plural: ''
+			  }
+			: rawNoun;
+
+	return (
+		<div>
+			{noun.article} {noun.singular}{' '} ({noun.plural})
+		</div>
+	);
 }
 
-export default Noun
+export default Noun;
